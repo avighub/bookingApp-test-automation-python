c6e910d4e84ab959d99c332a326bdff86838612c
Avishek Behera <avishekbehera87@gmail.com>
2021-12-06 17:55:15 +0530

    Updated README.md Added Tests to RestAPITest.py Added pipenv

--- README.md
+++ README.md
@@ -3,4 +3,8 @@
 #### Pre requisites
 - Install Python
 - Run the below command to install all required libraries
-```pip install -r dependencies.txt```
+```pip install -r requirements.txt```
+
+#### How to run tests
+- To run test from command line
+- ```pytest <pathOftheTestFile/TestFile.py```
--- com/users/RestAPITest.py
+++ com/users/RestAPITest.py
@@ -1,4 +1,7 @@
+import json
+
 import requests
+from pprint import pprint
 
 
 # GET Request
@@ -6,4 +9,46 @@ def test_send_get_request_and_verify_status_200_and_page_field_response():
     response = requests.get("https://reqres.in/api/users?page=1")
     assert response.status_code == 200
     response_body = response.json()
+    pprint(response_body)
     assert "page" in response_body
+
+
+def test_create_new_booking_and_verify_booking_info():
+    # Preparation
+    baseUrl = "https://restful-booker.herokuapp.com/booking"
+    createBooking_req_body = {
+        "firstname": "Deepa",
+        "lastname": "Seeba",
+        "totalprice": 111,
+        "depositpaid": True,
+        "bookingdates": {
+            "checkin": "2018-01-01",
+            "checkout": "2019-01-01"
+        },
+        "additionalneeds": "Breakfast"
+    }
+    createBooking_req_body_json = json.dumps(createBooking_req_body)
+
+    # Action
+    response_createBooking = requests.post(baseUrl, createBooking_req_body_json,
+                                           headers={"Content-Type": "application/json"})
+
+    # Debug
+    print("\nREQUEST URL: " + response_createBooking.request.url)
+    print("REQUEST BODY: " + response_createBooking.request.body)
+    print("\nREQUEST Headers: " + response_createBooking.request.headers.get("Content-Type"))
+    print("RESPONSE BODY:")
+    pprint(response_createBooking.json())
+
+    # Assertion
+    assert response_createBooking.status_code == 200
+    response_createBooking_json = response_createBooking.json()
+    bookingId = response_createBooking_json.get("bookingid")
+    assert response_createBooking_json["booking"]["firstname"] == "Deepa"
+
+    # get call to validate
+    url_get = f'{baseUrl}/{bookingId}'
+    print(url_get)
+    response_getBooking = requests.get(url_get).json()
+    pprint(response_getBooking)
+    assert response_getBooking["firstname"] == "Deepa"
