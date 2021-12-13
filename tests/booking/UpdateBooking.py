52b037cee45d893927b9f6732ebfa2c99d2a782c
Avishek Behera <avishekbehera87@gmail.com>
2021-12-13 09:34:02 +0530

    Added new tests for update , delete booking

---
+++ tests/booking/UpdateBooking.py
@@ -0,0 +1,44 @@
+import json
+
+import conftest
+from tests.booking import CreateBookingFunc
+import requests
+
+from utils import FileReader
+
+
+def test_Verify_bookingInfo_can_not_updated_without_token():
+    # Preparation
+    # Step1: Extract booking id
+    bookingId = CreateBookingFunc.createNewBookingAndReturnValue("bookingid")
+
+    # # Execution
+    updateUrlById = f'{conftest.BASE_URI}/{bookingId}'
+    updateBooking_req_body = FileReader.read_json_file("updateBookingRequest.json")
+    headers = {"Content-Type": "application/json", "token": "inavlidabcd123"}
+    response_updatedBooking = requests.put(url=updateUrlById, data=json.dumps(updateBooking_req_body), headers=headers)
+    print(response_updatedBooking.request.body)
+    print(response_updatedBooking.content)
+
+    # # Assertion
+    assert response_updatedBooking.status_code == 403
+    responseString = response_updatedBooking.content.decode("utf-8")
+    assert responseString == "Forbidden"
+
+def test_Verify_bookingInfo_can_not_partially_updated_without_token():
+    # Preparation
+    # Step1: Extract booking id
+    bookingId = CreateBookingFunc.createNewBookingAndReturnValue("bookingid")
+
+    # # Execution
+    updateUrlById = f'{conftest.BASE_URI}/{bookingId}'
+    updateBooking_req_body = FileReader.read_json_file("partialUpdateBookingRequest.json")
+    headers = {"Content-Type": "application/json", "token": "inavlidabcd123"}
+    response_updatedBooking = requests.put(url=updateUrlById, data=json.dumps(updateBooking_req_body), headers=headers)
+    print(response_updatedBooking.request.body)
+    print(response_updatedBooking.content)
+
+    # # Assertion
+    assert response_updatedBooking.status_code == 403
+    responseString = response_updatedBooking.content.decode("utf-8")
+    assert responseString == "Forbidden"
